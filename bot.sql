5ef778bd7c966815da163727ab8e2e3907d89dd1
DffDee1 <89968655+DffDee1@users.noreply.github.com>
2022-06-13 17:37:21 +0300

    Update and rename bot.db to bot.sql

---
+++ bot.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE messages (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+telegram_id INTEGER NOT NULL,
+text text NOT NULL
+);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
