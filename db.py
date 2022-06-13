09d2710dec5753bab0534dcdbd44dade1aa42ed1
DffDee1 <89968655+DffDee1@users.noreply.github.com>
2022-06-13 17:25:54 +0300

    Update db.py

--- db.py
+++ db.py
@@ -2,5 +2,5 @@ from databases import Database
 import os.path
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-db_path = os.path.join(BASE_DIR, "bot.db")
+db_path = os.path.join(BASE_DIR, "bot")
 database = Database('sqlite:///' + db_path)
