d0ab8fc634220508b55da48b131625e17e47ce04
DffDee1 <89968655+DffDee1@users.noreply.github.com>
2022-06-15 22:35:33 +0300

    Add files via upload

--- utils.py
+++ utils.py
@@ -5,8 +5,8 @@ class TestStates(Helper):
     mode = HelperMode.snake_case
 
     TEST_STATE_0 = ListItem()
-    TEST_STATE_1 = ListItem()
-    TEST_STATE_2 = ListItem()
+    first = ListItem()
+    second = ListItem()
     TEST_STATE_3 = ListItem()
     TEST_STATE_4 = ListItem()
     TEST_STATE_5 = ListItem()
