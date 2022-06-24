cab1a6d179f188d9ddae5585f9ade8d053ea51fa
DffDee1 <89968655+DffDee1@users.noreply.github.com>
2022-06-24 18:00:34 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from utils import *
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
-dp.middleware.setup(LoggingMiddleware()
+dp.middleware.setup(LoggingMiddleware())
 
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
